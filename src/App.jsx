bdf22b6fb745789a7ca3b13738886db5b0771a5d
bunty <bbunty2001@gmail.com>
2025-10-19 21:24:22 +0200

    feat: Creating Dashbord with side bar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
+import Dashboard from "./Dashboard";
+
 function App() {
-  return (
-    <div className="min-h-screen bg-green-100 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-green-700">
-        🌿 Tailwind funcionando correctamente ✅
-      </h1>
-      <p className="mt-4 text-gray-700">React + Vite + Tailwind listos para tu MVP.</p>
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default App;
