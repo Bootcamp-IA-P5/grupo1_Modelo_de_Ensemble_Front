d08e029587cef3f801d7f52af2991aae78012e98
bunty <bbunty2001@gmail.com>
2025-10-21 22:55:31 +0200

    Feat: added server option to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This tells Vite to listen on all public network interfaces
+    host: true, 
+    port: 5173, // Optional, but good to explicitly set
+  }
 })
+
