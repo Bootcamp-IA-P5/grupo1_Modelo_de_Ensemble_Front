efc8720e39a9b76fff2287ed36520c6dbc4ccea6
bunty <bbunty2001@gmail.com>
2025-10-22 09:10:22 +0200

    fix revoved network option

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  //server: {
     // This tells Vite to listen on all public network interfaces
-    host: true, 
-    port: 5173, // Optional, but good to explicitly set
-  }
+    //host: true, 
+    //port: 5173, // Optional, but good to explicitly set
+  //}
 })
 
